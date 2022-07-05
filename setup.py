e729baad39b824aa48bdabfa612e438b8e59ed07
Zhu Lin Ch'ng <z.chng@liverpool.ac.uk>
2022-07-05 22:46:25 +0800

    docs: added setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+"""Script for setuptools."""
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as readme:
+    long_description = readme.read()
+
+dependencies = [
+
+]
+
+setup(
+    name='floydwarshall',
+    version='0.0.1',
+    author='Zhu Lin Ch\'ng',
+    author_email='z.chng@liverpool.ac.uk',
+    description='Floyd-Warshall algorithm for shortest path',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://gitlab.csc.liv.ac.uk/sgzchng/floyd-warshall-algorithm',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=dependencies,
+)
