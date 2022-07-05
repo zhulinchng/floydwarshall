deb6f28ddc53cdf70f0a4e84723757eee43a3f4d
Zhu Lin Ch'ng <z.chng@liverpool.ac.uk>
2022-07-05 22:18:06 +0800

    refactor: added comments

--- visualization/visualize_graph.py
+++ visualization/visualize_graph.py
@@ -10,15 +10,18 @@ sys.path.insert(0, abspath(join(dirname(__file__), '..')))
 from floydwarshall import read_dir
 from visualization import make_graph
 
+# Read the directory of the graph
 destination_dir = 'visualization/files'
 viz_unittest_dir = destination_dir + '/unit_tests'
 viz_performance_test_dir = destination_dir + '/performance_tests'
 
+# Visualize unit tests graphs
 unit_tests_graphs = read_dir('tests/unit_tests/data')
 for i in unit_tests_graphs:
     make_graph(i['input']).save_graph(f'{viz_unittest_dir}/{i["case"]}_input.html')
     make_graph(i['output']).save_graph(f'{viz_unittest_dir}/{i["case"]}_output.html')
 
+# Visualize performance tests graphs
 folder_path = 'tests/performance_tests/data'
 perf_tests_graphs = [f for f in listdir(folder_path) if isfile(join(folder_path, f))]
 for graph in perf_tests_graphs:
