18c564fdfa31699c761c9d71e61325f2af2fede0
Zhu Lin Ch'ng <z.chng@liverpool.ac.uk>
2022-07-06 21:37:53 +0800

    style: added typing to the input for functions

--- tests/performance_test.py
+++ tests/performance_test.py
@@ -24,7 +24,7 @@ def perf_test(fn: callable) -> None:
                   'seed': 4} # this seed is specifically chosen to avoid negative cycles for these graphs
         fn(graph_generator(**kwargs))
 
-def timeit_perf_test(fn) -> None:
+def timeit_perf_test(fn: callable) -> None:
     """Run timeit test on the given Floyd-Warshall algorithm."""
     setup = """
 from __main__ import perf_test
@@ -37,7 +37,8 @@ from floydwarshall import fw_iterative, fw_recursive, fw_recursive_memo, graph_g
         f'perf_test({fn.__name__})', setup=setup, repeat=5, number=5)
     print(f'Time taken for {fn.__name__} is {min(time)} seconds')
 
-def cprofile_perf_test(fn) -> None:
+
+def cprofile_perf_test(fn: callable) -> None:
     """Run cprofile test on the given Floyd-Warshall algorithm."""
     setup = """
 from __main__ import perf_test
