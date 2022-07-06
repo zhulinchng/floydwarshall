6bd01c2289beee1cdb258d07d6d716414f8c9db2
Zhu Lin Ch'ng <z.chng@liverpool.ac.uk>
2022-07-06 20:27:25 +0800

    test: refactor unit_test with graph values
    replaced read_dir with graph values
    to import the tests package and run successfully

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -1,9 +1,10 @@
 """Unit test functions for the floyd warshall algorithm."""
 import unittest
 import sys
+from math import inf
 from os.path import dirname, join, abspath
 sys.path.insert(0, abspath(join(dirname(__file__), '..')))
-from floydwarshall import fw_iterative, fw_recursive, fw_recursive_memo, read_dir
+from floydwarshall import fw_iterative, fw_recursive, fw_recursive_memo
 
 
 class TestFloydWarshall(unittest.TestCase):
@@ -11,7 +12,15 @@ class TestFloydWarshall(unittest.TestCase):
 
     def test_floyd_warshall_success(self):
         """Equality test the floyd warshall algorithm on a set of test cases."""
-        tests = read_dir('tests/unit_tests/data')
+        tests = [{'case': 'case1',
+                  'input': [[0.0, 25.0], [-1.0, 0.0]],
+                  'output': [[0.0, 25.0], [-1.0, 0.0]]},
+                 {'case': 'case2',
+                  'input': [[0.0, 1.0, 43.0], [1.0, 0.0, 6.0], [-1.0, -1.0, 0.0]],
+                  'output': [[0.0, 1.0, 7.0], [1.0, 0.0, 6.0], [-1.0, -1.0, 0.0]]},
+                 {'case': 'case0',
+                  'input': [[0.0, 5.0, inf, 10.0], [inf, 0.0, 3.0, inf], [inf, inf, 0.0, 1.0], [inf, inf, inf, 0.0]],
+                  'output': [[0.0, 5.0, 8.0, 9.0], [inf, 0.0, 3.0, 4.0], [inf, inf, 0.0, 1.0], [inf, inf, inf, 0.0]]}]
         fn_list = [fw_iterative, fw_recursive, fw_recursive_memo]
         for fn in fn_list:
             for test in tests:
